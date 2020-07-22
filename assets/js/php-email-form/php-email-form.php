4e6ba6773b22baf6be67adf0857886d4bafdbcf5
Kwame Swift <fuakyeakyempim.charles@gmail.com>
2020-07-22 10:45:46 +0000

    Changes

--- assets/js/php-email-form/php-email-form.php
+++ assets/js/php-email-form/php-email-form.php
@@ -5,11 +5,11 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
+$form_content="From: $name \n Message: $message";
 $recipient = "fuakyeakyempim.charles@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+$mail_header = "From: $email \r\n";
+mail($recipient, $subject, $form_content, $mail_header) or die("Error!");
 echo "Thank You!";
 
 ?>
